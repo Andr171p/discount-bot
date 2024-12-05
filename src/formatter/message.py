cc72cfcec9ed4869834f0d6eaef87e07ed36c3f1
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-05 05:56:24 +0500

    [orders-status-app] 05.12.2024 bug fixing

--- src/formatter/message.py
+++ src/formatter/message.py
@@ -6,6 +6,7 @@ from src.app.schemas.order import OrderSchema
 
 
 async def get_message(order: Dict[str, Any]) -> str:
-    order = OrderSchema(**order)
+    # order = OrderSchema(**order)
     messages = await utils.load_json(path=config.messages.statuses)
-    return messages[order.status].format(**order.__dict__)
+    # return messages[order.status].format(**order.__dict__)
+    return messages[order['status']].format(order)
