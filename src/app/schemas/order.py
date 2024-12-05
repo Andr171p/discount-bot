f78bac7320d3f854d439090cbebc4b06994a8cf8
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-05 05:25:21 +0500

    [orders-status-app] 05.12.2024 add address formating+test

--- src/app/schemas/order.py
+++ src/app/schemas/order.py
@@ -56,3 +56,25 @@ class OrderSchema(BaseModel):
     @classmethod
     def validate_adress(cls, value: str) -> str:
         return utils.format_address(value)
+
+
+'''o = {
+  "client": "Косов Сергей Владимирович",
+  "number": "00НФ-013523",
+  "date": "2024-12-01T00:00:00",
+  "status": "Принят оператором",
+  "amount": 10,
+  "pay_link": "https://securepayments.tinkoff.ru/4J0Ek30r",
+  "pay_status": "NEW",
+  "cooking_time_from": "0001-01-01T00:00:00",
+  "cooking_time_to": "0001-01-01T13:30:00",
+  "delivery_time_from": "0001-01-01T15:00:00",
+  "delivery_time_to": "0001-01-01T15:30:00",
+  "project": "Дисконт Суши",
+  "trade_point": "Московский тракт, 87к1",
+  "trade_point_card": "MockoBcku'u TpakT 87 k.1 https://go.2gis.com/pdacd",
+  "delivery_method": "Курьер",
+  "delivery_adress": "625001, Тюменская обл, г.о. город Тюмень, г Тюмень, ул Полевая, д. 117, к. 2, кв. 145, подъезд 1, этаж 1",
+  "phones": ["9829764729"]
+}
+print(OrderSchema(**o))'''
