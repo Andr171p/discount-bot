bea0f80ccc4fd039dbf82e195681e16d70ab2921
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-05 07:22:21 +0500

    [orders-status-app] 05.12.2024 new message style + pay_link

--- src/app/routers/status.py
+++ src/app/routers/status.py
@@ -2,7 +2,8 @@ from aiogram import F, Router
 from aiogram.types import Message
 from typing import Any, Dict
 
-from src.app.keyboards.status import order_status_kb
+from src.app.keyboards.status import order_status_kb, pay_link_kb
+from src.app.schemas.order import OrderSchema
 from src.database.services.service import user_service
 from src.formatter.message import get_message
 from src.service import api
@@ -21,13 +22,20 @@ async def get_order_status(message: Message) -> None:
     orders: Dict[str, Any] = await api.get_user_orders(phone=phone)
     if len(orders['data']['orders']) != 0:
         for order in orders['data']['orders']:
-            text: str = await get_message(order=order)
-            await message.answer(
-                text=text,
-                reply_markup=await order_status_kb()
-            )
+            order = OrderSchema(**order)
+            text: str = await get_message(order=order.model_dump())
+            if order.pay_status != "CONFIRMED":
+                await message.answer(
+                    text=text,
+                    reply_markup=await pay_link_kb(url=order.pay_link)
+                )
+            else:
+                await message.answer(
+                    text=text,
+                    reply_markup=await order_status_kb()
+                )
     else:
-        text: Dict[str, str] = await load_json(path=config.messages.auth)
+        template: Dict[str, str] = await load_json(path=config.messages.auth)
         await message.answer(
-            text=text['empty']
+            text=template['empty']
         )
