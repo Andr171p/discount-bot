0b17c5857a9701cd8e4f015cfeef7cce24a6ac8f
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-05 05:29:05 +0500

    [orders-status-app] 05.12.2024 add address formating+test+

--- src/formatter/message.py
+++ src/formatter/message.py
@@ -8,5 +8,5 @@ from src.app.schemas.order import OrderSchema
 async def get_message(order: Dict[str, Any]) -> str:
     order = OrderSchema(**order)
     messages = await utils.load_json(path=config.messages.statuses)
-    print(messages)
+    print(messages[order.status].format(**order.__dict__))
     return messages[order.status].format(**order.__dict__)
