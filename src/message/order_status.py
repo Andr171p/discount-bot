94f1c0036d53b13659ad391bb6c263f1db832439
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-07 02:47:11 +0500

    [orders-status-app] 07.12.2024 fix status message

--- src/message/order_status.py
+++ src/message/order_status.py
@@ -21,7 +21,7 @@ class OrderStatus:
     async def _get_text(self) -> str:
         templates: Dict[str, Any] = await utils.load_json(path=config.messages.statuses)
         if self.order.delivery_method == "Принят оператором":
-            text: str = templates[self.order.status][self.order.delivery_method].format(self.order.model_dump())
+            text: str = templates[self.order.status][self.order.delivery_method].format(**self.order.model_dump())
             return text
         text: str = templates[self.order.status].format(**self.order.model_dump())
         return text
