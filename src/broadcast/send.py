127a728b7276d85cd5d4e7098d85395b2acb680f
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-06 18:11:31 +0500

    [orders-status-app] 06.12.2024 new message v1

--- src/broadcast/send.py
+++ src/broadcast/send.py
@@ -7,13 +7,26 @@ from src.formatter.message import get_message
 async def send_order_status(user_id: int, order: OrderSchema) -> None:
     text: str = await get_message(order=order.model_dump())
     if order.pay_status != "CONFIRMED" and order.status not in ("Завершен", "Отменен"):
-        await bot.send_message(
-            chat_id=user_id,
-            text=text,
-            reply_markup=await pay_link_kb(url=order.pay_link)
-        )
+        if order.delivery_method != "Курьер":
+            await bot.send_message(
+                chat_id=user_id,
+                text="Ваш заказ №{number} ПРИНЯТ время готовности {cooking_time_to} дата {date} г. по адресу: {trade_point_card}".format(**order.model_dump()),
+                reply_markup=await pay_link_kb(url=order.pay_link)
+            )
+        else:
+            await bot.send_message(
+                chat_id=user_id,
+                text=text,
+                reply_markup=await pay_link_kb(url=order.pay_link)
+            )
     else:
-        await bot.send_message(
-            chat_id=user_id,
-            text=f"{text}\nОПЛАЧЕН"
-        )
+        if order.delivery_method != "Курьер":
+            await bot.send_message(
+                chat_id=user_id,
+                text="Ваш заказ №{number} ПРИНЯТ время готовности {cooking_time_to} дата {date} г. по адресу: {trade_point_card}\nОПЛАЧЕН".format(**order.model_dump()),
+            )
+        else:
+            await bot.send_message(
+                chat_id=user_id,
+                text=f"{text}\nОПЛАЧЕН"
+            )
