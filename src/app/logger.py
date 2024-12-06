1fe17f6c13c4e3aab2e9258a5a2d50df947fc6a8
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-06 18:28:04 +0500

    [orders-status-app] 06.12.2024 update info logs

--- src/app/logger.py
+++ src/app/logger.py
@@ -3,7 +3,7 @@ import logging
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.DEBUG
+    level=logging.INFO
 )
 
 logger = logging.getLogger(
