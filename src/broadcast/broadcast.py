1ac7d460168aac97954f1a10dc578bd3b3c56491
AndreyKosov <andrey.kosov.05@inbox.ru>
2024-12-20 07:35:07 +0500

    [orders-status-app] 20.12.2024 [+]

--- src/broadcast/broadcast.py
+++ src/broadcast/broadcast.py
@@ -2,7 +2,12 @@ from src.rabbit_mq import RabbitConsumer
 from src.broadcast.process import process_message
 from src.config import settings
 
+import asyncio
+
 
 async def start_rabbit_broadcast() -> None:
     async with RabbitConsumer() as consumer:
+        await consumer.connect()
+        await consumer.channel()
         await consumer.consume_massage(process_message)
+        await asyncio.Future()
