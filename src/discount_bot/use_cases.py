481f6f20e8b9b4ac8b01afbe96deec9604f80006
AndreyKosov <andrey.kosov.05@inbox.ru>
2025-07-14 21:44:35 +0500

    14.07.2025 Add docstring to use cases

--- src/discount_bot/use_cases.py
+++ src/discount_bot/use_cases.py
@@ -3,6 +3,7 @@ from .base import ClientService, CrudRepository
 
 
 class RegistrationUseCase:
+    """Регистрация пользователя."""
     def __init__(self, user_repository: CrudRepository[User]) -> None:
         self._user_repository = user_repository
 
@@ -14,6 +15,7 @@ class RegistrationUseCase:
 
 
 class ReceivingOrdersUseCase:
+    """Получение заказов пользователя."""
     def __init__(
             self,
             client_service: ClientService,
